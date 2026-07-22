7ba0c56f557584290a5a8638ab1ed296188307b1
dubemernest23 <chidubemnnajiogbe23@gmail.com>
2026-07-22 23:19:57 +0100

    feat: fixed the ci error

--- internal/testUtils/db.go
+++ internal/testUtils/db.go
@@ -3,17 +3,19 @@ package testutils
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/mysqldialect"
 )
 
 func SetupTestDB() (*bun.DB, error) {
-	// dsn := "root:secret@tcp(localhost:3307)/mailforge_test?parseTime=true"
-	// dsn := os.Getenv("TEST_DB_DSN")
-	dsn := "root:secret@tcp(localhost:3307)/mailforge_test?parseTime=true&loc=UTC"
-	sqldb, err := sql.Open("mysql", dsn)
+	dsn := os.Getenv("TEST_DB_DSN")
+	if dsn == "" {
+		dsn = "root:secret@tcp(localhost:3307)/mailforge_test?parseTime=true&loc=UTC"
+	}
 
+	sqldb, err := sql.Open("mysql", dsn)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open test database: %w", err)
 	}
